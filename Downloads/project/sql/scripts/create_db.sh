89e4a2894e0249c4694057ee7942a5d7a0bf7d79
Alex Hsieh <alexchsieh@yahoo.com>
2023-03-11 18:34:54 -0800

    Revert "initial commit"
    This reverts commit 32e7292578dc3259aa20cf02a63a03fd0a7cb6e9.

--- Downloads/project/sql/scripts/create_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_tables.sql
-psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_indexes.sql
-psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/load_data.sql
-
