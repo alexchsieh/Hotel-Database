d4c616fe454939d79f052f038a45e9a81c2604bb
Alex Hsieh <alexchsieh@yahoo.com>
2023-03-11 21:13:02 -0800

    added aboslute data path

--- Project/sql/src/load_data.sql
+++ Project/sql/src/load_data.sql
@@ -1,36 +1,36 @@
 COPY Users
-FROM 'users.csv' 
+FROM '/extra/$USER/project/data/users.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101; 
 
 COPY Hotel
-FROM 'hotels.csv'
+FROM '/extra/$USER/project/data/hotels.csv' 
 WITH DELIMITER ',' CSV HEADER;   
 
 COPY Rooms
-FROM 'rooms.csv'
+FROM '/extra/$USER/project/data/rooms.csv' 
 WITH DELIMITER ',' CSV HEADER;
 
 COPY MaintenanceCompany
-FROM 'company.csv'
+FROM '/extra/$USER/project/data/company.csv' 
 WITH DELIMITER ',' CSV HEADER;
 
 COPY RoomBookings
-FROM 'bookings.csv'
+FROM '/extra/$USER/project/data/bookings.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE RoomBookings_bookingID_seq RESTART 501; 
 
 COPY RoomRepairs
-FROM 'roomRepairs.csv'
+FROM '/extra/$USER/project/data/roomRepairs.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairs_repairID_seq RESTART 11;
 
 COPY RoomRepairRequests
-FROM 'roomRepairRequests.csv'
+FROM '/extra/$USER/project/data/roomRepairRequests.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairRequests_requestNumber_seq RESTART 11;
 
 COPY RoomUpdatesLog
-FROM 'roomUpdatesLog.csv'
+FROM '/extra/$USER/project/data/roomUpdatesLog.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomUpdatesLog_updateNumber_seq RESTART 51;
