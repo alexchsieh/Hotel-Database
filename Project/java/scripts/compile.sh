205584d5b74bc39fe5b2014fbf691af603b9cbf5
nipkip <nkhalil942@gmail.com>
2023-03-13 06:59:41 +0000

    added pg port var in compile.sh

--- Project/java/scripts/compile.sh
+++ Project/java/scripts/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+export PGPORT=8192
 export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
 export PATH=$JAVA_HOME/bin:$PATH
 
@@ -10,4 +10,3 @@ javac -d $DIR/../classes $DIR/../src/Hotel.java
 #run the java program
 #Use your database name, port number and login
 java -cp $DIR/../classes:$DIR/../lib/pg73jdbc3.jar Hotel $USER"_DB" $PGPORT $USER
-
